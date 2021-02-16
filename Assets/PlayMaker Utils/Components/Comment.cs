d8d2b166d224212ab70466baf97a996f1f8d56ee
Jean Fabre <contact@fabrejean.net>
2021-02-16 11:22:25 +0300

    removed playmaker utils, will be replaced by sub module

--- Assets/PlayMaker Utils/Components/Comment.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace HutongGames.PlayMaker.Ecosystem.utils
-{
-	public class Comment : MonoBehaviour {
-		public string Text;
-	}
-}
--- Assets/PlayMaker Utils/Internal/EventDataSenderProxy.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-using HutongGames.PlayMaker;
-
-public class EventDataSenderProxy : ScriptableObject {
-
-	public FsmEventTarget EventTarget = new FsmEventTarget ();
-}
